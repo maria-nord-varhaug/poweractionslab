8b33b9b8cd1a50afd7dae52bb6d317d06a743a85
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-15 14:00:00 +0100

    add script for downloading package deployer and create necessary file structure

---
+++ deployPackage.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy -ExecutionPolicy AllSigned
+Install-Module Microsoft.Xrm.Tooling.PackageDeployment.Powershell    
+Get-CrmPackages -PackageDirectory "C:\Users\marino\Documents\github_actions_test\poweractionslab\DealerUI installation v.0.0.28\weu-dev-develop\Tools"
---
+++ preparePackageDeploymentAndUnzipPackage.ps1
@@ -0,0 +1,40 @@
+param ($path, $environment)
+$knownPartOfFileName = "Annata365Dataverse."
+$file = Get-ChildItem -Filter "$knownPartOfFileName*.zip" | Select-Object -First 1
+if ($file)
+  {
+    $pattern = "[0-9]+\.[0-9]+\.[0-9]+"
+    $versionNumber = [regex]::Match($file.FullName, $pattern).Value
+    $folderName = "DealerUI installation v." + $versionNumber + "\" + $environment
+
+    mkdir $folderName
+    Set-Location $folderName
+
+    $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+    $targetNugetExe = ".\nuget.exe"
+    Remove-Item .\Tools -Force -Recurse -ErrorAction Ignore
+    Invoke-WebRequest $sourceNugetExe -OutFile $targetNugetExe
+    Set-Alias nuget $targetNugetExe -Scope Global -Verbose
+
+    ##
+    ##Download Package Deployer
+    ##
+    ./nuget install Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O .\Tools
+    mkdir .\Tools\PackageDeployment
+    $pdFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
+    Move-Item .\Tools\$pdFolder\tools\*.* .\Tools\PackageDeployment
+    Remove-Item .\Tools\$pdFolder -Force -Recurse
+
+    ##
+    ##Remove NuGet.exe
+    ##
+    Remove-Item nuget.exe
+
+    $extractionPath = $path + "\" + $foldername +"\Tools"
+
+    # Use Expand-Archive to extract the file
+    Expand-Archive -Path $file.FullName -DestinationPath $extractionPath
+  } else
+  {
+    Write-Host "No file found starting with $knownPartOfFileName"
+  }
