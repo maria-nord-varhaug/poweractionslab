feabfb845ba0f1a75777d96d99572609a924b0d8
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-15 15:25:17 +0100

    hopefully fix

--- preparePackageDeploymentAndUnzipPackage.ps1
+++ preparePackageDeploymentAndUnzipPackage.ps1
@@ -6,9 +6,9 @@ if ($file)
     $pattern = "[0-9]+\.[0-9]+\.[0-9]+"
     $versionNumber = [regex]::Match($file.FullName, $pattern).Value
     $folderName = "DealerUI installation v." + $versionNumber + "\" + $environment
-
     mkdir $folderName
     Set-Location $folderName
+    $rootDir = Get-Location
 
     $sourceNugetExe = "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
     $targetNugetExe = ".\nuget.exe"
@@ -22,7 +22,7 @@ if ($file)
     ./nuget install Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O .\Tools
     mkdir .\Tools\PackageDeployment
     $pdFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
-    Move-Item .\Tools\$pdFolder.Name\tools\*.* .\Tools\PackageDeployment
+    Move-Item "$rootDir\Tools\$pdFolder.Name\tools\*.*" "$rootDir\Tools\PackageDeployment"
     Remove-Item .\Tools\$pdFolder -Force -Recurse
 
     ##
