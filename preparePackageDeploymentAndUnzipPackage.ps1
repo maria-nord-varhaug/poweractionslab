2ee72152d2aeffa8cb79a14801365a34d0e23124
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-15 15:06:19 +0100

    Attempt to fix error

--- preparePackageDeploymentAndUnzipPackage.ps1
+++ preparePackageDeploymentAndUnzipPackage.ps1
@@ -22,7 +22,7 @@ if ($file)
     ./nuget install Microsoft.CrmSdk.XrmTooling.PackageDeployment.WPF -O .\Tools
     mkdir .\Tools\PackageDeployment
     $pdFolder = Get-ChildItem ./Tools | Where-Object {$_.Name -match 'Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.'}
-    Move-Item .\Tools\$pdFolder\tools\*.* .\Tools\PackageDeployment
+    Move-Item .\Tools\$pdFolder.Name\tools\*.* .\Tools\PackageDeployment
     Remove-Item .\Tools\$pdFolder -Force -Recurse
 
     ##
