1b257497c5fe67c66c4cb4ff73ac41f5ba86dc3a
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-17 12:45:26 +0100

    add missing variables

--- deployPackage.ps1
+++ deployPackage.ps1
@@ -1,4 +1,5 @@
-param ($environment, $versionNumber)
+param ($environment, $fileVersion)
+$startDir = Get-Location
 Set-ExecutionPolicy -ExecutionPolicy AllSigned
 Install-Module Microsoft.Xrm.Tooling.PackageDeployment.Powershell    
-Get-CrmPackages -PackageDirectory "$startDir\DealerUI installation v.$versionNumber\$environment\Tools"
+Get-CrmPackages -PackageDirectory "$startDir\DealerUI installation v.$fileVersion\$environment\Tools"
