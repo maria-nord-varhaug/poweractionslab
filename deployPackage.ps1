5c6de960a1ab64c77ee954232f4229c7970ca41d
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-17 13:55:10 +0100

    fix it up

--- deployPackage.ps1
+++ deployPackage.ps1
@@ -1,5 +1,3 @@
 param ($environment, $fileVersion)
 $startDir = Get-Location
-Set-ExecutionPolicy -ExecutionPolicy AllSigned
-Install-Module Microsoft.Xrm.Tooling.PackageDeployment.Powershell    
 Get-CrmPackages -PackageDirectory "$startDir\DealerUI installation v.$fileVersion\$environment\Tools"
