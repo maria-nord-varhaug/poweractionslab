5dfe9a742ee812888eede05895766bb6a0ef787b
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-17 12:41:33 +0100

    d

--- preparePackageDeploymentAndUnzipPackage.ps1
+++ preparePackageDeploymentAndUnzipPackage.ps1
@@ -1,5 +1,6 @@
 param ($environment, $fileVersion)
-$file = Get-ChildItem -Filter "Annata365Dataverse.*.zip" | Select-Object -First 1
+$fileName = "Annata365Dataverse.$fileVersion.zip"
+$file = Get-ChildItem -Filter "$fileName" | Select-Object -First 1
 if ($file)
   {
     $startDir = Get-Location
@@ -16,8 +17,8 @@ if ($file)
     Move-Item "$tools\Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.*\tools\*.*" "$tools\PackageDeployment"
     Remove-Item "$tools\Microsoft.CrmSdk.XrmTooling.PackageDeployment.Wpf.*" -Force -Recurse
     Remove-Item "$workingDir\nuget.exe"
-    Expand-Archive -Path "$startDir\$knownPartOfFileName*.zip" -DestinationPath "$tools"
+    Expand-Archive -Path "$startDir\$fileName" -DestinationPath "$tools"
     } else
   {
-    Write-Host "No file found starting with $knownPartOfFileName"
+    Write-Host "No file found called $fileName"
   }
