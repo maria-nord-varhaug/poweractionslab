8613417ce2fe5701b3eb0f4be47d81b4b8787d8f
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-22 14:43:38 +0100

    run

--- deployPackage.ps1
+++ deployPackage.ps1
@@ -3,3 +3,4 @@ $loc = Get-Location
 $file = Get-ChildItem -Filter "Annata365Dataverse.*.pdpkg.zip" | Select-Object -First 1
 $fileVersion = [regex]::Match($file.FullName, "[0-9]+\.[0-9]+\.[0-9]+").Value
 Get-CrmPackages -PackageDirectory "$loc\DealerUI installation v.$fileVersion\$environment\Tools"
+Get-CrmPackages -PackageDirectory "c:\Users\marino\dploy_dv\DealerUI installation v.0.1.1\weu-dev-develop\tools"
