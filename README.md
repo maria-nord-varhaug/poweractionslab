6b51a8394f33e82a70e1dcb4223ac4bea7a12457
Maria Nord-Varhaug <mnordvh@gmail.com>
2024-01-22 15:09:27 +0100

    Hope for a better tomorrow

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# poweractionslab
+# Power platform package deployment
+
+This project automatically deploys zip files created with the package deployer, using the package deployer for windows terminal. 
--- deployPackage.ps1
+++ deployPackage.ps1
@@ -1,6 +1,11 @@
-param($environment)
-$loc = Get-Location
-$file = Get-ChildItem -Filter "Annata365Dataverse.*.pdpkg.zip" | Select-Object -First 1
-$fileVersion = [regex]::Match($file.FullName, "[0-9]+\.[0-9]+\.[0-9]+").Value
-Get-CrmPackages -PackageDirectory "$loc\DealerUI installation v.$fileVersion\$environment\Tools"
-Get-CrmPackages -PackageDirectory "c:\Users\marino\dploy_dv\DealerUI installation v.0.1.1\weu-dev-develop\tools"
+param($username, [SecureString] $password)
+Import-Module Microsoft.Xrm.Tooling.CrmConnector.PowerShell
+$PSVersionTable.PSVersion
+echo $env:PSModulePath
+$cred = New-Object System.Management.Automation.PSCredential ("$username", ("$password"))
+$conn = Get-CrmConnection -Credential $cred -ServerUrl "operations-weu-dev-develop.crm4.dynamics.com"
+if($conn.IsReady) {
+  Write-Output "Connected successfully to Dataverse"
+} else {
+  Write-Output "Failed to connect to Dataverse"
+}
